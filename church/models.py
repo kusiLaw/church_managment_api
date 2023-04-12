38300aacfd086f7d18ecccbffccdf4bdb6841d38
kusi <lawcubegsm@gmail.com>
2023-04-12 17:41:12 +0000

    Add image field to member model

--- church/models.py
+++ church/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+def image_path(instance, filename):
+  return 'members/image/'.format(instance.user.username, filename)
+
 class Member(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   number = models.CharField(max_length=20, blank=True)
@@ -9,6 +12,8 @@ class Member(models.Model):
   postal_code = models.CharField(max_length=20 )
   address_line = models.CharField(max_length=25)
   date_baptized = models.DateField(blank= True)
+  image = models.ImageField(max_length=100, upload_to=image_path, null=True, blank= True )
+
 
   def __str__(self) -> str:
     return f'{self.user.first_name}  {self.user.last_name}'
