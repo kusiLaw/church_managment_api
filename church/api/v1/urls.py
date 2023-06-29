899cc20c7eaa1fad3b90efdd74c0a2c8004d64ce
kusi <lawcubegsm@gmail.com>
2023-06-29 16:52:29 +0000

    Fix link issue

--- church/api/v1/urls.py
+++ church/api/v1/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import EventsList, UsersList
+from .views import EventsList, UserList
 
 app_name = 'church'
 
 urlpatterns = [
-    path('v1/users', UsersList.as_view(), name='users'),
-    path('v1/user/<int:pk>/', views.user_detail, name='user_detail'),
+    path('v1/users/', UserList.as_view(), name='userlist'),
+    # path('v1/user/<int:pk>/', views.user_detail, name='user_detail'),
     path('v1/events/', EventsList.as_view(), name='event'),
 ]
