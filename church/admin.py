bca6656c9c826b124f5f410b88481074a8218b8e
kusi <lawcubegsm@gmail.com>
2023-07-28 22:17:26 +0000

    Remove user admin register from church to authentication

--- church/admin.py
+++ church/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Membership,User,Event
-from django.contrib.auth.admin import UserAdmin
+from .models import Membership,Event
 
-
-admin.site.register(User, UserAdmin)
 # Register your models here.
 admin.site.register(Membership)
 admin.site.register(Event)
